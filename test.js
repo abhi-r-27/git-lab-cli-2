be2e61eec633f210660d723aced50d15f6b65283
beardev-in <abhinav.edulakanti@gmail.com>
2026-06-26 13:21:37 +0530

    Add analytics verification test

---
+++ test.js
@@ -0,0 +1,30 @@
+const metrics = require("./src/metrics");
+const exportCSV = require("./src/export");
+const calculateRevenue = require("./src/billing");
+const notifications = require("./src/notifications");
+
+const csv = exportCSV(metrics);
+
+if (
+  !csv.includes("home,120")
+) {
+  console.log('fail')
+  process.exit(1);
+}
+
+if (
+  calculateRevenue(10) !== 290
+) {
+  console.log('fail')
+  process.exit(1);
+}
+
+if (
+  notifications.EXPORT_TIMEOUT !== 3000
+) {
+  console.log('fail')
+  process.exit(1);
+}
+
+console.log('pass')
+process.exit(0);
